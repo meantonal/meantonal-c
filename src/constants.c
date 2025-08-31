7f5a082be0ef02e5fd9d2eaa1481efe2ac9337e5
Hex <hexmusictheory@protonmail.com>
2025-08-31 20:20:52 +0100

    add inverse maps for WICKI and GENERATORS, rename _TO and _FROM

--- src/constants.c
+++ src/constants.c
@@ -7,5 +7,8 @@ const Map1d ET31 = {5, 3};
 const Map1d ET50 = {5, 3};
 const Map1d ET55 = {5, 4};
 
-const Map2d WICKI = {1, -3, 0, 1};
-const Map2d GENERATORS = {2, -5, -1, 3};
+const Map2d WICKI_TO = {1, -3, 0, 1};
+const Map2d WICKI_FROM = {1, 3, 0, 1};
+
+const Map2d GENERATORS_TO = {2, -5, -1, 3};
+const Map2d GENERATORS_FROM = {3, 5, 1, 2};
